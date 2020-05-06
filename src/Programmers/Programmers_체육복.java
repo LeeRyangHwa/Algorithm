028dda1328d8cede7a48ca743e7321ed8cf65ebe
LeeRyangHwa <iryanghwa@gmail.com>
2020-05-06 12:49:00 +0900

    체육복

--- src/Programmers/Programmers_체육복.java
+++ src/Programmers/Programmers_체육복.java
@@ -2,49 +2,38 @@ package Programmers;
 
 public class Programmers_체육복 {
 
-        public int solution(int n, int[] lost, int[] reserve) {
-            int answer = 0;
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int re_count=0;
+        int count = 0;
 
-            for(int i=0; i<lost.length; i++){
-                for(int j=0; j<reserve.length; j++){
-                    if(lost[i]==reserve[j]){
-                        lost[i]=0;
-                        reserve[j]=0;
-                    }
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]==reserve[j]){
+                    lost[i]=-1;
+                    reserve[j]=-1;
+                    re_count++;
+                    break;
                 }
             }
-
-            int loststu=0;
-            //int r=0;
-
-            for(int i=0; i<lost.length; i++){
-                int check=0;
-                int lostnum = lost[i];
-                if(lostnum!=0){
-                    for(int j=0; j<reserve.length; j++){
-                        if(lostnum == reserve[j]-1||reserve[j]+1==lostnum){
-                            reserve[j]=0;
-                            check=1;
-                        }
-                    }
-                }
-                if(check!=1){
-                    loststu++;
-                }
-            }
-
-            for(int l: lost){
-                System.out.print(l+" ");
-            }
-            System.out.println("");
-            for(int r: reserve){
-                System.out.print(r+" ");
-            }
-
-            System.out.println("");
-            System.out.println(loststu);
-            return answer;
         }
+
+
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]==reserve[j]+1 || lost[i]==reserve[j]-1){
+
+                    reserve[j]=-1;
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        answer = n-lost.length+re_count+count;
+
+        return answer;
+    }
 
 
 }
